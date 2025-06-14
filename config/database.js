5aa19b2bd57cee5c96ff14e3197207f339449112
iamshaswat <shaswatkumar281@bbdu.ac.in>
2025-06-14 12:28:51 +0530

    update

--- config/database.js
+++ config/database.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
+
 require('dotenv').config();
 
 const dbConnection = async () => {
     mongoose.connect(process.env.DATABASE_URL,)
-    .then(() => console.log('Connected to MongoDB'))
+    .then(() => console.log(`Connected to MongoDB ${process.env.DATABASE_URL}`))
     .catch((err) => console.log('Failed to connect to MongoDB', err))
 }
 
